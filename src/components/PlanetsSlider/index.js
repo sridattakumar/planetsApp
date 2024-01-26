440cde86a2dca5aedfc85a2d9fdfd68a14da91d1
sridattakumar <dattakumar1999@gmail.com>
2024-01-26 10:40:09 +0000

    solution

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,27 @@
+// Write your code here
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
+  return (
+    <div className="planets-conatiner" data-testId="planets">
+      <h1 className="heading">PLANETS</h1>
+      <Slider {...settings}>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+export default PlanetsSlider
